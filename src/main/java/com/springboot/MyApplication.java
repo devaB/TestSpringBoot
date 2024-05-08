747b5eddbbd61a930d5a5b6dc76c8f855116c6ce
devaB <devendrabarai141@gmail.com>
2024-05-08 23:16:38 +0530

    dev

--- src/main/java/com/springboot/MyApplication.java
+++ src/main/java/com/springboot/MyApplication.java
@@ -3,6 +3,7 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @AutoConfiguration
+@EnableJpaRepositories
 //@EnableSwagger2
 public class MyApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
--- src/main/java/com/springboot/repository/CountriesRepository.java
+++ src/main/java/com/springboot/repository/CountriesRepository.java
@@ -4,6 +4,6 @@ import com.springboot.entity.Countries;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "countries")
+@RepositoryRestResource(path = "countriesapi")
 public interface CountriesRepository extends JpaRepository<Countries, Integer> {
 }
